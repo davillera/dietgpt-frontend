5fe80b24cc784427c3dcbe45f8704bbc548bacf9
Daniel Villera <davillera77@gmail.com>
2024-07-05 11:47:47 -0500

    Update page.tsx

--- src/app/recommendation/page.tsx
+++ src/app/recommendation/page.tsx
@@ -1,8 +1,31 @@
 "use client";
+import Loading from "./loaging"; // Asegúrate de importar tu componente de carga
+
+import Link from "next/link";
 import { useFormContext } from "../context/FormContext";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import Loading from "./loaging"; // Asegúrate de importar tu componente de carga
+
+interface FormData {
+  // Define aquí la interfaz de tu FormData según la estructura que tengas
+}
+
+interface PlanNutricional {
+  desayuno: string;
+  media_mañana: string;
+  almuerzo: string;
+  merienda: string;
+  cena: string;
+}
+
+interface ApiResponse {
+  completion: {
+    plan_nutricional: PlanNutricional;
+    plan_entrenamiento: {
+      [key: string]: string;
+    };
+  };
+}
 
 export default function Page() {
   const { FormData } = useFormContext();
@@ -11,37 +34,17 @@ export default function Page() {
   const API = "http://localhost:3300";
   const router = useRouter();
   const [loading, setLoading] = useState(false);
-  // const [response, setResponse] = useState(null);
-  let dataResponse: any;
+  const [response, setResponse] = useState<ApiResponse | null>(null);
 
   useEffect(() => {
     FormData ? getRecommendation() : router.push("/");
   }, [FormData, API, router]);
 
-  // const data = {
-  //   plan_nutricional: {
-  //     desayuno: "Batido de proteínas con plátano y avena",
-  //     media_mañana: "Yogur griego con frutos secos",
-  //     almuerzo: "Pechuga de pollo a la plancha con quinoa y verduras al vapor",
-  //     merienda: "Tortilla de claras con espinacas",
-  //     cena: "Salmón al horno con batata asada",
-  //   },
-  //   plan_entrenamiento: {
-  //     día_1:
-  //       "Entrenamiento de fuerza: Sentadillas, press de banca, peso muerto, dominadas",
-  //     día_2:
-  //       "Entrenamiento de hipertrofia: Extensiones de cuádriceps, curl de bíceps, elevaciones laterales, abdominales",
-  //     día_3: "Descanso activo: Caminata ligera o yoga",
-  //     día_4:
-  //       "Entrenamiento de fuerza: Prensa de piernas, press militar, remo con barra, fondos en paralelas",
-  //   },
-  // };
-
-  function getRecommendation() {
+  async function getRecommendation() {
     console.log(API);
 
     setLoading(true); // Inicia el estado de carga
-    fetch(`${API}/openai/completion`, {
+    await fetch(`${API}/openai/completion`, {
       method: "POST",
       body: JSON.stringify(FormData),
       headers: {
@@ -50,9 +53,8 @@ export default function Page() {
     })
       .then((response) => response.json())
       .then((data) => {
-        dataResponse = data;
         console.log(data, "desde el fetch");
-        // setResponse(data); // Guarda la respuesta
+        setResponse(data); // Guarda la respuesta
         setLoading(false); // Finaliza el estado de carga
       })
       .catch((error) => {
@@ -61,95 +63,99 @@ export default function Page() {
       });
   }
 
+  if (loading || !response) {
+    return <Loading />;
+  }
 
+  const { plan_nutricional, plan_entrenamiento } = response.completion;
 
   return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className="bg-green min-h-screen flex flex-col justify-center items-center text-black">
-          <h1 className="text-4xl font-bold mb-8 mt-4 font-space-grotesk">
-            Tu Plan Nutricional y de Ejercicio
+    <div className="bg-green min-h-screen flex flex-col justify-center items-center text-black">
+      <h1 className="text-4xl text-center font-bold mb-8 mt-4 font-space-grotesk">
+        Tu Plan Nutricional y de Ejercicio
+      </h1>
+      <div className="bg-gray-100 min-h-screen p-4 w-full">
+        <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-6">
+          <h1 className="text-3xl font-bold text-gray-800 mb-6">
+            Plan Nutricional
           </h1>
-          <div className="bg-gray-100 min-h-screen p-4">
-            <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-6">
-              <h1 className="text-3xl font-bold text-gray-800 mb-6">
-                Plan Nutricional
-              </h1>
 
-              <div className="mb-8">
-                <h2 className="text-xl font-semibold text-gray-700 mb-2">
-                  Desayuno
-                </h2>
-                <p className="text-gray-600">
-                  {/* {dataResponse.completion.plan_nutricional.desayuno} */}
-                </p>
-              </div>
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-700 mb-2">
+              Desayuno
+            </h2>
+            <p className="text-gray-600">{plan_nutricional.desayuno}</p>
+          </div>
 
-              <div className="mb-8">
-                <h2 className="text-xl font-semibold text-gray-700 mb-2">
-                  Media Mañana
-                </h2>
-                <p className="text-gray-600">
-                  {/* {dataResponse.plan_nutricional.media_mañana} */}
-                </p>
-              </div>
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-700 mb-2">
+              Media Mañana
+            </h2>
+            <p className="text-gray-600">{plan_nutricional.media_mañana}</p>
+          </div>
 
-              <div className="mb-8">
-                <h2 className="text-xl font-semibold text-gray-700 mb-2">
-                  Almuerzo
-                </h2>
-                <p className="text-gray-600">
-                  {/* {dataResponse.plan_nutricional.almuerzo} */}
-                </p>
-              </div>
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-700 mb-2">
+              Almuerzo
+            </h2>
+            <p className="text-gray-600">{plan_nutricional.almuerzo}</p>
+          </div>
 
-              <div className="mb-8">
-                <h2 className="text-xl font-semibold text-gray-700 mb-2">
-                  Merienda
-                </h2>
-                <p className="text-gray-600">
-                  {/* {dataResponse.plan_nutricional.merienda} */}
-                </p>
-              </div>
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-700 mb-2">
+              Merienda
+            </h2>
+            <p className="text-gray-600">{plan_nutricional.merienda}</p>
+          </div>
 
-              <div className="mb-8">
-                <h2 className="text-xl font-semibold text-gray-700 mb-2">
-                  Cena
-                </h2>
-                <p className="text-gray-600">
-                  {/* {dataResponse.plan_nutricional.cena} */}
-                </p>
-              </div>
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold text-gray-700 mb-2">Cena</h2>
+            <p className="text-gray-600">{plan_nutricional.cena}</p>
+          </div>
 
-              <h1 className="text-3xl font-bold text-gray-800 mb-6">
-                Plan de Entrenamiento
-              </h1>
+          <h1 className="text-3xl font-bold text-gray-800 mb-6">
+            Plan de Entrenamiento
+          </h1>
 
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Día
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Entrenamiento
-                    </th>
+          <div className="overflow-x-auto">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Día
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Entrenamiento
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {Object.entries(plan_entrenamiento).map(([day, workout]) => (
+                  <tr key={day}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {day}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {workout}
+                    </td>
                   </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200"></tbody>
-              </table>
-            </div>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
-      )}
-    </>
+      </div>
+      <div>
+        <button className="my-4 px-6 py-3 bg-black text-white rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600">
+          <Link href={"./form"}>¿Hacemos Otro?</Link>
+        </button>
+      </div>
+    </div>
   );
 }
